1cd58bd4ac648da73b56bd44a319b4004edbaf8b
Abdul Rasheed <rasheedpri@gmail.com>
2021-12-28 08:02:26 +0000

    Fortigate Module

--- modules/fgtvm/vairables.tf
+++ modules/fgtvm/vairables.tf
@@ -1,6 +1,31 @@
-variable "fw_name"  {}
+# ResourceGroup Variables
+
 variable "location" {}
 variable "resource_group_name"  {}
+
+# FortiGate Virtual Machine Variables
+variable "fw_name"  {}
 variable "nic1_subnet_id"   {}
 variable "nic2_subnet_id"   {}
 variable "storage_account_id"   {}
+
+# Subnet Variables
+variable "subnet_name"   {}
+variable "vnet_name"  {}
+variable "address_prefixes"  {}
+variable "nsg_name"   {}
+
+# NSG Rule Variables
+
+variable "nsgrule" {
+  default = [
+    {
+      direction    = "Outbound"},
+    {
+      direction    = "Inbound"
+    }
+  ]
+}
+
+
+
